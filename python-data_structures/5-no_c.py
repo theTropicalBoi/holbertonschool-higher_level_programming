26f1d451f4f74082c9ec14d338d21d57be0ab199
theTropicalBoi <shaans1920@icloud.com>
2025-01-27 00:06:56 +0400

    Task 5 and 8

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i == 'c' or i == 'C':
-            continue
-        new_string = ""
-        for i in my_string:
-            if i != 'c' and i != 'C':
-                new_string += i
-        return new_string
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != "c" and my_string[i] != "C":
+            new_string += my_string[i]
+    return new_string
