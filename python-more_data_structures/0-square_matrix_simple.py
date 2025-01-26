004eea2e8527d434946725e2fed2f59b40dae0da
theTropicalBoi <shaans1920@icloud.com>
2025-01-27 00:09:40 +0400

    Project - More Data Structure

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if len(matrix) == 0:
+        return matrix
+    new_matrix = [[element**2 for element in row] for row in matrix]
+    return new_matrix
