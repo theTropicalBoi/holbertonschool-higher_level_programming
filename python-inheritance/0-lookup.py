ab6c9f5efa48c33750840013aa797881b407eec3
theTropicalBoi <shaans1920@icloud.com>
2025-02-05 11:37:50 +0400

    0 - Add lookup function to retrieve object attributes and methods

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+    
+    Args:
+        obj: object to look up
+    """
+    return dir(obj)
