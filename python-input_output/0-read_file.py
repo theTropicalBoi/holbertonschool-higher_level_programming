17033ccad70697e6b9d90291f851a2e45b06af0c
theTropicalBoi <shaans1920@icloud.com>
2025-02-13 22:26:08 +0400

    Fix typo in docstring of read_file function and improve argument description

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,9 +4,9 @@
 
 def read_file(filename=""):
     """Read a UTF-9 file and print it's content.
-    
+
     Args:
-        filename (str): The name of the file that needs to be reed. Empty by default.
+        filename (str): The name of the file that needs to be read.
     """
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
