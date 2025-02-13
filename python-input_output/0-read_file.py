8e10275b4dd3bcff94223671fbe1b2598557b085
theTropicalBoi <shaans1920@icloud.com>
2025-02-13 22:24:17 +0400

    Add docstring and comments to read_file function for clarity

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
+"""This is a module for reading file."""
+
+
 def read_file(filename=""):
+    """Read a UTF-9 file and print it's content.
+    
+    Args:
+        filename (str): The name of the file that needs to be reed. Empty by default.
+    """
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")
