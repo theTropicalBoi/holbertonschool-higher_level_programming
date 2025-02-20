a37deb36286eec9b5e488b35a8c20f8c408cf55f
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-02-20 22:14:39 +0400

    Refactor fetch_and_print_posts and fetch_and_save_posts for clarity and improved functionality

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
 
+
 import requests
 import csv
 
-"""
-Use requests library to request and process http responses,
-Parses and manipulate JSON data using Python.
-"""
 
-
+"""
+Function to interact with the request library
+"""
 def fetch_and_print_posts():
     """
-    Fetch all posts from JSON Placeholder
-    Print Status Code if 200 and parse it to JSON Object
-    Print out the file title.
+    Fetches all posts from JSONPlaceHolder
+    print the status code of the response.
+    If the request is successful, parse the fetched data into a JSON obj.
+    Iterate through the parsed JSON data and print out the titles of all the posts.
     """
-    response = requests.get(url="https://jsonplaceholder.typicode.com/posts")
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {response.status_code}")
+
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
             print(post['title'])
 
-
 def fetch_and_save_posts():
     """
-    Fetch all post from JSON PlaceHolder
-    If request is successful, structure the data into a list of dicts
-    where each dict represents a post with keys and values for id,
+    Fetches all post from JSONPlaceHolder
+    If request if successful, instead of printing titles, structure the data
+    into a list of dicts, where each dict represents a post with keys and values for id,
     title and body.
     """
-    response = requests.get(url="https://jsonplaceholder.typicode.com/posts")
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
 
     if response.status_code == 200:
         posts = response.json()
-        structured = [{'id': post['id'], 'title': post['title'],
-                       'body': post['body']} for post in posts]
+        structured_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
 
         with open('posts.csv', 'w', newline='', encoding='utf-8') as file:
             writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-
             writer.writeheader()
-            writer.writerows(structured)
+            for post in structured_data:
+                writer.writerow(post)
