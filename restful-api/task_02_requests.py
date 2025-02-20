280d5fd07171310fce46eaace36198b515b36b63
Shaan Somarchand <153312506+theTropicalBoi@users.noreply.github.com>
2025-02-20 19:48:27 +0400

    Refactor fetch_and_save_posts to use writerows for improved efficiency

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -37,6 +37,6 @@ def fetch_and_save_posts():
 
         with open('posts.csv', 'w', newline='', encoding='utf-8') as file:
             writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+
             writer.writeheader()
-            for post in structured:
-                writer.writerow(post)
+            writer.writerows(structured)
